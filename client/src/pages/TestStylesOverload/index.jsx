9b3827b33430391cdc49d8a84b1da3b8a65a49ff
BEGEMOT9I <daston2012@gmail.com>
2018-01-30 21:35:24 +0300

    added tso

---
+++ client/src/pages/TestStylesOverload/index.jsx
@@ -0,0 +1,37 @@
+// @flow
+import React, { Component } from 'react'
+import injectSheet from 'react-jss'
+
+import styles, { COUNT_ELEMENTS } from './styles'
+
+type Props = {
+  classes: Object
+}
+type State = {}
+
+class TestStylesOverload extends Component<Props, State> {
+  elements: Array<number>
+
+  constructor(props: Props) {
+    super(props)
+
+    this.elements = []
+
+    for (let i = 0; i < COUNT_ELEMENTS; i += 1) {
+      this.elements.push(i)
+    }
+  }
+  render() {
+    const { classes } = this.props
+
+    return (
+      <section className={classes.container}>
+        {this.elements.map(index => (
+          <div key={`tco-${index}`} className={classes[`tco-${index}`]} />
+        ))}
+      </section>
+    )
+  }
+}
+
+export default injectSheet(styles)(TestStylesOverload)
---
+++ client/src/pages/TestStylesOverload/styles.js
@@ -0,0 +1,22 @@
+// @flow
+
+export const COUNT_ELEMENTS = 10000
+
+const styles = {
+  container: {
+    display: 'flex',
+    'flex-wrap': 'wrap',
+    width: '100%',
+    height: '100%'
+  }
+}
+
+for (let i = 0; i < COUNT_ELEMENTS; i += 1) {
+  styles[`tco-${i}`] = {
+    width: '20px',
+    height: '20px',
+    'background-color': `rgb(0, ${Math.round(i / 255) % 255}, ${i % 255})`
+  }
+}
+
+export default styles
