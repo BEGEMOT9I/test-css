0b69de337edd2145d5529bb169835814449faa0f
BEGEMOT9I <daston2012@gmail.com>
2018-01-30 21:21:33 +0300

    added tso

---
+++ client/src/pages/TestStylesOverload/index.jsx
@@ -0,0 +1,32 @@
+// @flow
+import React, { Component } from 'react'
+
+import classes from './styles.scss'
+
+type Props = {}
+type State = {}
+
+class TestStylesOverload extends Component<Props, State> {
+  elements: Array<number>
+
+  constructor(props: Props) {
+    super(props)
+
+    this.elements = []
+
+    for (let i = 0; i < 10000; i += 1) {
+      this.elements.push(i)
+    }
+  }
+  render() {
+    return (
+      <section className={classes.container}>
+        {this.elements.map(index => (
+          <div key={`tco-${index}`} className={classes[`tco-${index}`]} />
+        ))}
+      </section>
+    )
+  }
+}
+
+export default TestStylesOverload
